0c9ff4a281751def94145cbdae45827345a42177
adelazzi <adel2002azzi@gmail.com>
2024-06-12 11:45:27 +0100

    done

--- backend/dilivary/dilivary/accounts/middleware.py
+++
@@ -1,9 +0,0 @@
-# your_app_name/middleware.py
-from django.utils.deprecation import MiddlewareMixin
-from django.utils import timezone
-
-class UpdateLastSeenMiddleware(MiddlewareMixin):
-    def process_view(self, request, view_func, view_args, view_kwargs):
-        if request.user.is_authenticated:
-            request.user.last_seen = timezone.now()
-            request.user.save()
--- backend/dilivary/dilivary/location/serializers.py
+++
@@ -1,8 +0,0 @@
-# serializers.py
-from rest_framework import serializers
-from .models import UserLocation
-
-class UserLocationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserLocation
-        fields = ['ids', 'latitude', 'longitude','geolocation', 'timestamp']
